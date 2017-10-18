a77fbf9f161ad470637d111a14a7a1542e3e7be1
westnorthern <rrichardson610@gmail.com>
2017-10-18 13:56:05 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+$(function(){
+
+
+$(document).on('click', '#fortune', function(){
+	console.log("fortune");
+
+	});
+
+$(document).on('click', '#hamantash', function(){
+	console.log("hamantash");
+	});
+
+$(document).on('click', '#oreo', function(){
+	console.log("oreo");
+
+	});
+
+
+});
