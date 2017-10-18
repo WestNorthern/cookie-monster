b1773d35f5cdb8e6dcb68ab0351c8d239a5ec189
westnorthern <rrichardson610@gmail.com>
2017-10-18 16:49:17 -0600

    basic functionality for cookie counter and basic cookie divs

--- index.js
+++ index.js
@@ -1,20 +1,57 @@
 
+function displayCookies(){
+	$('#fortune-num').html(Cookies.get('fortune'));
+	$('#hamantash-num').html(Cookies.get('hamantash'));
+	$('#oreo-num').html(Cookies.get('oreo'));
+}
+
+
+function eraseCookies(){
+	if (Cookies.get('fortune') !== undefined){
+		Cookies.set('fortune', 0, { expires: 600 });
+	}
+
+	if (Cookies.get('hamantash') !== undefined){
+		Cookies.set('hamantash', 0, { expires: 600 });
+	}
+
+	if (Cookies.get('oreo') !== undefined){
+		Cookies.set('oreo', 0, { expires: 600 });
+	}
+}
+
+	
+
+
 $(function(){
 
+	displayCookies();
 
-$(document).on('click', '#fortune', function(){
-	console.log("fortune");
+	$(document).on('click', '#fortune', function(){
+		console.log("fortune");
+		Cookies.set('fortune', parseInt(Cookies.get('fortune')) + 1);
+		displayCookies();
 
-	});
+		});
 
-$(document).on('click', '#hamantash', function(){
-	console.log("hamantash");
-	});
+	$(document).on('click', '#hamantash', function(){
+		console.log("hamantash");
+		Cookies.set('hamantash', parseInt(Cookies.get('hamantash')) + 1);
+		displayCookies();
+		});
 
-$(document).on('click', '#oreo', function(){
-	console.log("oreo");
+	$(document).on('click', '#oreo', function(){
+		console.log("oreo");
+		Cookies.set('oreo', parseInt(Cookies.get('oreo')) + 1);
+		displayCookies();
 
-	});
+		});
+
+	$(document).on('click', '#erase', function(){
+		eraseCookies();
+		displayCookies();
+
+		});
 
 
 });
