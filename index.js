da97fc674de7b6b8e475bfbf8d9ea4407706370e
westnorthern <rrichardson610@gmail.com>
2017-10-19 16:13:11 -0600

    added abduction animation for all cookies

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ $(function(){
 		});
 
 
-	$(document).on('click', '#fortune-cookie', function(){
+	$(document).on('click', '#fortune-jar', function(){
     let fortuneC = $("#fortune-cookie-wrapper");
     $('#laser-one').animate({
     	opacity: '1'
@@ -65,7 +65,7 @@ $(function(){
     		top: '-500px',
     		width: '30px',
     		height: '30px'
-    	})
+    	}, 1000)
     .animate(
     {
     	opacity: '0'
@@ -84,6 +84,70 @@ $(function(){
     $('#laser-one').animate({
     	opacity: '0'
     });
+	});
+
+	$(document).on('click', '#rainbow-jar', function(){
+    let fortuneC = $("#rainbow-wrapper");
+    $('#laser-three').animate({
+    	opacity: '1'
+    });
+    fortuneC.animate(
+    	{
+    		left: '500px', 
+    		top: '-500px',
+    		width: '30px',
+    		height: '30px'
+    	}, 1000)
+    .animate(
+    {
+    	opacity: '0'
+    })
+    .animate(
+    	{top: '-200px',
+    	 left: '850px',
+    	 width: '150px',
+    	 height: '150px'
+    })
+    .animate(
+    {
+    	opacity: '1.0'
+    });
+    console.log('Boooo');
+    $('#laser-three').animate({
+    	opacity: '0'
+    });
+	});
+
+	$(document).on('click', '#chip-jar', function(){
+    let fortuneC = $("#chip-wrapper");
+    $('#laser-two').animate({
+    	opacity: '1'
+    });
+    fortuneC.animate(
+    	{
+    		left: '500px', 
+    		top: '-500px',
+    		width: '30px',
+    		height: '30px'
+    	}, 1000)
+    .animate(
+    {
+    	opacity: '0'
+    })
+    .animate(
+    	{top: '-200px',
+    	 left: '450px',
+    	 width: '150px',
+    	 height: '150px'
+    })
+    .animate(
+    {
+    	opacity: '1.0'
+    });
+    console.log('Boooo');
+    $('#laser-two').animate({
+    	opacity: '0'
+    }, 1000);
 	});
 
 
