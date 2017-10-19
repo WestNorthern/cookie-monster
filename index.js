3b3da5d7a98bdbe263f7e001da08b7249f40103f
westnorthern <rrichardson610@gmail.com>
2017-10-19 17:45:29 -0600

    added textures to containers, linked cookies to page

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 
 function displayCookies(){
-	$('#fortune-num').html(Cookies.get('fortune'));
-	$('#hamantash-num').html(Cookies.get('hamantash'));
-	$('#oreo-num').html(Cookies.get('oreo'));
+	$('#chip-counter').text(Cookies.get('fortune'));
+	$('#fortune-counter').text(Cookies.get('hamantash'));
+	$('#rainbow-counter').text(Cookies.get('oreo'));
 }
 
 
 function eraseCookies(){
 	if (Cookies.get('fortune') !== undefined){
-		Cookies.set('fortune', 0, { expires: 600 });
+		Cookies.set('fortune', 0);
 	}
 
 	if (Cookies.get('hamantash') !== undefined){
-		Cookies.set('hamantash', 0, { expires: 600 });
+		Cookies.set('hamantash', 0);
 	}
 
 	if (Cookies.get('oreo') !== undefined){
-		Cookies.set('oreo', 0, { expires: 600 });
+		Cookies.set('oreo', 0);
 	}
 }
 
@@ -25,22 +25,34 @@ function eraseCookies(){
 
 $(function(){
 
+	if (Cookies.get('fortune') == undefined){
+		Cookies.set('fortune', 0);
+	}
+
+	if (Cookies.get('hamantash') == undefined){
+		Cookies.set('hamantash', 0);
+	}
+
+	if (Cookies.get('oreo') == undefined){
+		Cookies.set('oreo', 0);
+	}
+
 	displayCookies();
 
-	$(document).on('click', '#fortune', function(){
+	$(document).on('click', '#chip-jar', function(){
 		console.log("fortune");
 		Cookies.set('fortune', parseInt(Cookies.get('fortune')) + 1);
 		displayCookies();
 
 		});
 
-	$(document).on('click', '#hamantash', function(){
+	$(document).on('click', '#fortune-jar', function(){
 		console.log("hamantash");
 		Cookies.set('hamantash', parseInt(Cookies.get('hamantash')) + 1);
 		displayCookies();
 		});
 
-	$(document).on('click', '#oreo', function(){
+	$(document).on('click', '#rainbow-jar', function(){
 		console.log("oreo");
 		Cookies.set('oreo', parseInt(Cookies.get('oreo')) + 1);
 		displayCookies();
@@ -137,8 +149,8 @@ $(function(){
     .animate(
     	{top: '-200px',
     	 left: '450px',
-    	 width: '150px',
-    	 height: '150px'
+    	 width: '125px',
+    	 height: '125px'
     })
     .animate(
     {
